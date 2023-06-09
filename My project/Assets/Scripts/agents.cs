f847470da5c8fbae7ea604d3cfcb474d7036689d
greenhairavocado <harry.xu2024@gmail.com>
2023-06-08 18:31:23 -0700

    Created ML scene

--- My project/Assets/Scripts/agents.cs
+++ My project/Assets/Scripts/agents.cs
@@ -8,31 +8,52 @@ using Unity.MLAgents.Sensors;
 public class agents : Agent
 {
     public float speed;
+    public Material success;
+    public Material failure;
+    public MeshRenderer floor;
     public Transform goalPosition;
-    public override void OnEpisodeBegin()
+    
+    public override void OnEpisodeBegin()   //start position 
     {
-        transform.localPosition = new Vector3(1f, 2f, 4f);
+        transform.localPosition = new Vector3(1f, 2f, 4f);      
     }
 
-    public override void CollectObservations(VectorSensor sensor)
+    public override void CollectObservations(VectorSensor sensor)    //AI's vision
     {
         sensor.AddObservation(transform.localPosition);
-        sensor.AddObservation(goalPosition.localPosition);
+        sensor.AddObservation(goalPosition.localPosition);      
     }
 
-    public override void OnActionReceived(ActionBuffers actions)
+    public override void OnActionReceived(ActionBuffers actions)     //AI action
     {
         float xVelocity = actions.ContinuousActions[0];
         float zVelocity = actions.ContinuousActions[1];
-        transform.localPosition += new Vector3(xVelocity, 0, zVelocity) * Time.deltaTime * speed;
+        transform.localPosition += new Vector3(xVelocity, 0, zVelocity) * Time.deltaTime * speed;   
     }
 
-    public override void Heuristic(in ActionBuffers action)
+    public override void Heuristic(in ActionBuffers action)   //User control
     {
-        ActionSegment<float> continuousActions = action.ContinuousActions;
+        ActionSegment<float> continuousActions = action.ContinuousActions;     
         continuousActions[0] = Input.GetAxisRaw("Horizontal");
         continuousActions[1] = Input.GetAxisRaw("Vertical");
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log($"We collided with {other}");
+        Debug.Log(other.gameObject.tag);
+        if (other.gameObject.tag == "Goal_tag")
+        {
+            SetReward(1f);
+            floor.material = success;
+            EndEpisode();
+        }
+        if (other.gameObject.tag == "wall") 
+        {
+            SetReward(-1f);
+            floor.material = failure;
+            EndEpisode();
+        }
+    }
 
 }
